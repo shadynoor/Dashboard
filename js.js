28117e10e889c08ed416574aee3b245041bc238a
shadynoor <shadynoor9@gmail.com>
2022-11-10 17:49:07 +0200

    Modified Notifcation Circle And Make it Dynamic

--- js.js
+++ js.js
@@ -1,8 +1,46 @@
-var i;
-var y=6
-var x = ["b",1,2,5]
-for ( i = 0; i < x.length; i++) {
-    y+=x[i]
+var clickOnNotf = document.querySelectorAll(".notfbar")
+// console.log(clickOnNotf);
+
+// for (let i = 0; i < clickOnNotf.length; i++) {
+
+//     var notfCircle = window.getComputedStyle(
+//         document.querySelector('.notfbar'), '::before'
+//     )
+//     clickOnNotf[i].addEventListener("click", () => {
+//         notfCircle.setProperty("display", "none");
+//     })
+    
+// }
+
+// var notfCircle = window.getComputedStyle(
+// 	document.querySelector('.notfbar'), '::before'
+// )
+
+// // box.addEventListener("click" , (e) => {
+// //     console.log(e);
+// // })
+
+var notificationBell = document.getElementById("notification")
+
+var notificationList = document.querySelector(".notification")
+
+var clickOnNotf = document.querySelectorAll(".notfbar")
+
+var indicator = document.querySelector(".indicator")
+var notfindicator = document.querySelectorAll(".notfindicator")
+
+notificationBell.addEventListener("click" , () => {
+    indicator.style.display = "none";
+    if (notificationList.style.display == "inline") {
+        notificationList.style.display = "none"
+    }else{
+        notificationList.style.display = "inline"
+    }
+})
+
+for (let i = 0; i < clickOnNotf.length; i++) {
+    clickOnNotf[i].addEventListener("click",() => {
+        notfindicator[i].style.display = "none"
+    })
     
 }
-console.log(y);
